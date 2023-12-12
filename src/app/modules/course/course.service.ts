f825dbec54b52b9d37f804fed9c96076f23545a7
mottakinrahat <rahatchowhdury661@gmail.com>
2023-12-12 19:19:48 +0600

    dataadded

--- src/app/modules/course/course.service.ts
+++ src/app/modules/course/course.service.ts
@@ -38,8 +38,32 @@ const updateCourseIntoDB = async (id: string, payload: Partial<TCourse>) => {
     { new: true, runValidators: true }
   );
 
-  if(tags && tags.length > 0) {
-    const deletedTags=tags.filter(el=>el.name && el.isDeleted)
+  if (tags && tags.length > 0) {
+    const deletedTags = tags.filter((el) => el.name && el.isDeleted);
+    const deleteOperation = await Course.findByIdAndUpdate(
+      id,
+      {
+        $set: {
+          "tags.$[elem].isDeleted": true,
+        },
+      },
+      {
+        new: true,
+        arrayFilters: [
+          { "elem.name": { $in: deletedTags.map((tag) => tag.name) } },
+        ],
+        runValidators: true,
+      }
+    );
+
+    //add new data
+    const addTags: object = tags.filter((el) => el.name && !el.isDeleted);
+    const addToSetOperation = await Course.findByIdAndUpdate(
+      id,
+      { $addToSet: { tags: { $each: addTags } } },
+      { new: true, runValidators: true }
+    );
+    return addToSetOperation;
   }
   return updateBasicCourseInfo;
 };
